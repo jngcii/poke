57a68580e96a860dd40389e06387e334c04f5e33
jngcii <concotree@gmail.com>
2021-09-25 06:26:21 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# POKE
+
+### poke what you think
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <div>
+        <h1>POKE SERVICE DEVELOPMENT START!</h1>
+    </div>,
+    document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
