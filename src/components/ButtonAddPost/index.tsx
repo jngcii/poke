453670236e93505926e7fbac6ffba942aadb6f1e
jngcii <concotree@gmail.com>
2021-10-16 23:09:24 +0900

    feature-21 post 추가 로직에 key 계산 로직 추가

--- src/components/ButtonAddPost/index.tsx
+++ src/components/ButtonAddPost/index.tsx
@@ -4,6 +4,7 @@ import { addPost } from '../../redux/slices/postSlice';
 import './style.scss';
 import { RootState } from '../../redux/store';
 import { createPost } from '../../redux/utils/objectCreator';
+import { key8Factory } from '../../redux/utils/keyFactory';
 
 export default React.memo(() => {
   const { posts } = useSelector((state: RootState) => state.post);
@@ -11,8 +12,14 @@ export default React.memo(() => {
   const dispatch = useDispatch();
 
   const onAdd = () => {
+    const sortedKeyList = [...posts.map((it) => it.id)].sort(key8Factory.compare);
+
+    const newKey = sortedKeyList.length > 0
+      ? key8Factory.first(sortedKeyList[0])
+      : key8Factory.init();
+
     const newPost = createPost(
-      'abcdefgh',
+      newKey,
       'New List',
     );
     dispatch(addPost(newPost));
