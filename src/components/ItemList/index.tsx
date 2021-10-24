ab3b304f32e0c35f962fe616592511581a824628
jngcii <concotree@gmail.com>
2021-10-24 23:57:38 +0900

    feature-23 item 추가/수정/삭제 기능 추가

--- src/components/ItemList/index.tsx
+++ src/components/ItemList/index.tsx
@@ -1,13 +1,20 @@
-import React, {ChangeEvent, useCallback, useEffect, useState} from 'react';
-import { useDispatch } from 'react-redux';
+import React, {
+  ChangeEvent, KeyboardEvent, useCallback, useEffect, useRef, useState,
+} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useDraggable } from 'muuri-react';
-import {checkItem, updateItem} from '../../redux/slices/itemSlice';
+import {
+  addItem, checkItem, removeItem, updateItem,
+} from '../../redux/slices/itemSlice';
 import { Item as ItemInterface } from '../../types/object';
 import './style.scss';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import ItemGrid from '../ItemGrid';
+import { RootState } from '../../redux/store';
+import { key8Factory } from '../../redux/utils/keyFactory';
+import { createItem } from '../../redux/utils/objectCreator';
 
 export default React.memo(({ items }: ItemsProps) => {
   const children = items.map((item) => <Item key={item.id} item={item} />);
@@ -50,26 +57,49 @@ export const ItemCheckbox = React.memo(({ item, onCheck }: ItemCheckboxProp) => 
 
 const ItemContent = React.memo(({ item }: ItemProp) => {
   const [text, setText] = useState(item.content);
+  const inputRef = useRef(null);
+  const { items } = useSelector((state: RootState) => state.item);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!text || !text.trim()) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  const onKeyDown = useCallback((e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      const sortedOrders = items.map((it) => it.order).sort((a, b) => key8Factory.compare(a, b));
+      const itemIndex = sortedOrders.indexOf(item.order);
+      const nextOrder = itemIndex === items.length - 1 ? undefined : sortedOrders[itemIndex + 1];
+      const newOrder = key8Factory.build(item.order, nextOrder);
+      const newItem = createItem(newOrder, item.postId, '', false);
+      dispatch(addItem(newItem));
+    }
+  }, [items]);
 
   const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
-
-    console.log(text);
 
     e.preventDefault();
   }, [text]);
 
   const onBlur = useCallback(() => {
-    const newItem = { ...item, content: text };
-    console.log(newItem);
-    dispatch(updateItem({ id: item.id, item: newItem }));
+    if (!text || !text.trim()) {
+      dispatch(removeItem(item.id));
+    } else {
+      const newItem = { ...item, content: text };
+      dispatch(updateItem({ id: item.id, item: newItem }));
+    }
   }, [item, text]);
 
   return (
     <div className="component-item-content">
       <input
+        ref={inputRef}
         value={text}
+        onKeyDown={onKeyDown}
         onChange={onChange}
         onBlur={onBlur}
       />
