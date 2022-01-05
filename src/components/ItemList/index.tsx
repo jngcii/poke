b2a67e17dfcf680cf497904519a0edfe2f50adf7
jngcii <concotree@gmail.com>
2022-01-06 00:46:11 +0900

    feature-24 itemList는 testablility가 떨어져 상세히 주석

--- src/components/ItemList/index.tsx
+++ src/components/ItemList/index.tsx
@@ -61,36 +61,46 @@ const ItemContent = React.memo(({ item }: ItemProp) => {
   const { items } = useSelector((state: RootState) => state.item);
   const dispatch = useDispatch();
 
+  // 리렌더링 될 때 text가 비어있다 : 새로 생긴 ItemContent 컴퍼넌트다
+  // 그때는 input에 포커싱이 된다.
   useEffect(() => {
     if (!text || !text.trim()) {
       inputRef.current.focus();
     }
   }, []);
 
-  const onKeyDown = useCallback((e: KeyboardEvent<HTMLInputElement>) => {
+  const onEnter = useCallback((e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       e.preventDefault();
 
+      // enter 시 현재 ItemContent의 text가 비어있으면 취소와 같은 동작을 한다.
+      // = 현재 ItemContent 제거!
       if (!text || !text.trim()) {
         dispatch(removeItem(item.id));
         return;
       }
 
+      // 전체 items 중 현재 ItemContent의 위치를 찾는다.
       const sortedOrders = items.map((it) => it.order).sort((a, b) => key8Factory.compare(a, b));
       const itemIndex = sortedOrders.indexOf(item.order);
+      // 만약 마지막이라면 현재 다음 순서가 없기에 현재와 다음 사이에 껴넣을수 없다는 것이고, 그래서 nextOrder를 undefined로 설정한다.
       const nextOrder = itemIndex === items.length - 1 ? undefined : sortedOrders[itemIndex + 1];
       const newOrder = key8Factory.build(item.order, nextOrder);
       const newItem = createItem(newOrder, item.postId, '', false);
+      // dispatch하면 store.item.items state에 하나가 추가되면서 Post가 리렌더링되고,
+      // 결국 ItemList 및 ItemContent도 리렌더링된다.
+      // (이때 새로생긴 ItemContent는 text가 없기 때문에 input focusing된다.)
       dispatch(addItem(newItem));
     }
   }, [items]);
 
-  const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+  const onChangeText = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
 
     e.preventDefault();
   }, [text]);
 
+  // Blur 당시 Item이 빈문자가 아닐 때에만 Item Update Dispatch
   const onBlur = useCallback(() => {
     if (!text || !text.trim()) {
       dispatch(removeItem(item.id));
@@ -105,14 +115,18 @@ const ItemContent = React.memo(({ item }: ItemProp) => {
       <input
         ref={inputRef}
         value={text}
-        onKeyDown={onKeyDown}
-        onChange={onChange}
+        onKeyDown={onEnter}
+        onChange={onChangeText}
         onBlur={onBlur}
       />
     </div>
   );
 });
 
+/**
+ * Drag를 언제 할수 있는지는 조금더 고민해보고 기능 리팩터링 가능
+ * e.g. hover를 오래하고 있으면 drag 안내말풍선 + 롱클릭하면 드래그 시작처럼 가능
+ */
 const ItemDragger = React.memo(() => {
   const draggable = useDraggable();
 
