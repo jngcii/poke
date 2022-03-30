9fe208bfc556639c4c7b9859b08ba37a55d8f463
jngcii <concotree@gmail.com>
2022-03-30 21:23:08 +0900

    feature-48 spare memory fix 기능 작성

--- src/components/MemorySpareList/index.tsx
+++ src/components/MemorySpareList/index.tsx
@@ -3,10 +3,12 @@ import React, {
   useMemo,
   useState,
 } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Memory } from '../../types/object';
 import { RootState } from '../../redux/store';
 import './style.scss';
+import { key8Factory } from '../../redux/utils/keyFactory';
+import { updateMemory } from '../../redux/slices/memorySlice';
 
 export default React.memo(() => {
   const [currentMemories, setCurrentMemories] = useState([]);
@@ -30,9 +32,25 @@ const MemorySpareItem = React.memo(({ memory }: MemoryProps) => {
   const {
     memory: { memories },
   } = useSelector((state: RootState) => state);
+  const dispatch = useDispatch();
 
   const onFix = () => {
-    console.log('fix toggled.');
+    const fixedMemories = memories.filter((it) => it.fixed);
+
+    let order;
+
+    if (fixedMemories.length === 0) {
+      order = key8Factory.build();
+    } else {
+      const sortedMemories = fixedMemories.length === 1
+        ? fixedMemories
+        : fixedMemories.sort((before, after) => key8Factory.compare(before.order, after.order));
+      const lastMemory = sortedMemories[sortedMemories.length - 1];
+      order = key8Factory.build(lastMemory.order, undefined);
+    }
+
+    const newMemory = { ...memory, order, fixed: true };
+    dispatch(updateMemory({ id: memory.id, memory: newMemory }));
   };
 
   return (
