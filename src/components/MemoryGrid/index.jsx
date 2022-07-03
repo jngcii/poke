4ec75e6b0c749bf5c9b3b2baa91a49bd76c4a0f9
jngcii <concotree@gmail.com>
2022-07-04 00:04:05 +0900

    feature-49 MemoryGrid(= MemoryChildGrid + MemoryParentGrid) 리팩터링

--- src/components/MemoryGrid/index.jsx
+++ src/components/MemoryGrid/index.jsx
@@ -5,17 +5,20 @@ import { key8Factory } from '../../redux/utils/keyFactory';
 import gridItemReplacer from '../../redux/utils/gridItemReplacer';
 import { updateMemory } from '../../redux/slices/memorySlice';
 
-export default React.memo(({ children }) => {
+export default React.memo(({ children, parent }) => {
+  const hasParent = !!parent;
+  const parentId = hasParent ? parent.id : '0';
+
   const { memories } = useSelector((state) => state.memory);
   const dispatch = useDispatch();
 
   const sortedMemories = useMemo(
     () => (memories.length > 1
       ? [...memories].filter(
-        (it) => it.parentId === '0' && it.fixed,
+        (it) => it.parentId === parentId && it.fixed,
       ).sort(
         (before, after) => key8Factory.compare(before.order, after.order),
-      ) : [...memories].filter((it) => it.parentId === '0' && it.fixed)),
+      ) : [...memories].filter((it) => it.parentId === parentId && it.fixed)),
     [memories],
   );
 
@@ -34,7 +37,7 @@ export default React.memo(({ children }) => {
 
   return (
     <MuuriComponent
-      {...gridProps}
+      {...getGridProps(hasParent)}
       onDragEnd={changeTargetKey}
       onDragStart={recordCurrentPosition}
       propsToData={({ memory }) => memory}
@@ -45,21 +48,25 @@ export default React.memo(({ children }) => {
   );
 });
 
-const gridProps = {
-  dragEnabled: true,
-  dragFixed: true,
-  dragSortHeuristics: {
-    sortInterval: 0,
-  },
-  dragAxis: 'y',
+const getGridProps = (hasParent) => {
+  const prefix = hasParent ? 'child' : 'parent';
 
-  // ClassNames
-  containerClass: 'component-memory-parent-list-container',
-  itemClass: 'component-memory-parent-item-outer',
-  itemVisibleClass: 'component-memory-parent-item-outer-shown',
-  itemHiddenClass: 'component-memory-parent-item-outer-hidden',
-  itemPositioningClass: 'component-memory-parent-item-outer-positioning',
-  itemDraggingClass: 'component-memory-parent-item-outer-dragging',
-  itemReleasingClass: 'component-memory-parent-item-outer-releasing',
-  itemPlaceholderClass: 'component-memory-parent-item-outer-placeholder',
+  return {
+    dragEnabled: true,
+    dragFixed: true,
+    dragSortHeuristics: {
+      sortInterval: 0,
+    },
+    dragAxis: 'y',
+
+    // ClassNames
+    containerClass: `component-memory-${prefix}-list-container`,
+    itemClass: `component-memory-${prefix}-item-outer`,
+    itemVisibleClass: `component-memory-${prefix}-item-outer-shown`,
+    itemHiddenClass: `component-memory-${prefix}-item-outer-hidden`,
+    itemPositioningClass: `component-memory-${prefix}-item-outer-positioning`,
+    itemDraggingClass: `component-memory-${prefix}-item-outer-dragging`,
+    itemReleasingClass: `component-memory-${prefix}-item-outer-releasing`,
+    itemPlaceholderClass: `component-memory-${prefix}-item-outer-placeholder`,
+  };
 };
